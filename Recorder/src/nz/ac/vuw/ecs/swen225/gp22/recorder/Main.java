db960f80e49cd760311f4430faae913b546ab994
Santino Gaeta <gaetasant@myvuw.ac.nz>
2022-09-07 14:47:23 +1200

    Trying to get git to register correct Username

--- Recorder/src/nz/ac/vuw/ecs/swen225/gp22/recorder/Main.java
+++ Recorder/src/nz/ac/vuw/ecs/swen225/gp22/recorder/Main.java
@@ -2,6 +2,6 @@ package nz.ac.vuw.ecs.swen225.gp22.recorder;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Username set hopefully");
     }
 }

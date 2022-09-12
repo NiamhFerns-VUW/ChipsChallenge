55625311f762d36bfd2e5758c164b660186e1f67
Niamh Ferns <fernsniam@myvuw.ac.nz>
2022-09-12 18:54:27 +1200

    GameClock is now almost fully done. Just need to implement the timed bit of it in the onTick lambda. Once that's done it should in theory be usable.

--- src/nz/ac/vuw/ecs/swen225/gp22/app/GameClock.java
+++ src/nz/ac/vuw/ecs/swen225/gp22/app/GameClock.java
@@ -1,47 +1,71 @@
 package nz.ac.vuw.ecs.swen225.gp22.app;
 
-public class GameClock {
+import java.util.ArrayList;
+import java.util.List;
+
+public class GameClock implements Subject {
     private static GameClock clock;
+    private final ArrayList<Observer> timedElements; // Move the observer stuff to a separate class.
     private static final Runnable onTick = () -> {
         // Do something to determine time to wait for a tick... Need to talk to Borgar about it.
-        get().tickIncrement();
+        while(get().running) {
+            get().tickIncrement();
+
+            // Tick observers.
+            get().update();
+        }
     };
     private static final Thread tickThread = new Thread(onTick, "Tick Thread");
 
-    private long tick;
-    private long levelTick;
-    private long time;
-    private long levelTime;
+    private long tickCount;
+    private long levelTickCount;
+    private long timePlayed;
+    private long timeRemaining;
+    private boolean running;
 
     public static GameClock get() {
         if (clock == null) clock = new GameClock();
         return clock;
     }
 
-    public long currentTick()      { return tick; }
-    public long currentTime()      { return time; }
-    public long currentLevelTick() { return levelTick; }
-    public long currentLevelTime() { return levelTime; }
+    @Override
+    public void update() {
+        List.copyOf(timedElements).forEach(Observer::update);
+    }
+    @Override
+    public void register(Observer ob) {
+        timedElements.add(ob);
+    }
+    @Override
+    public void unregister(Observer ob) {
+        timedElements.remove(ob);
+    }
+
+    public long currentTick()      { return tickCount; }
+    public long currentTime()      { return timePlayed; }
+    public long currentLevelTick() { return levelTickCount; }
+    public long currentLevelTime() { return timeRemaining; }
 
     protected void setLevelTime(long time) {
-        this.time = time;
+        this.timeRemaining = time;
     }
     protected  void tickIncrement() {
-        levelTick++;
-        tick++;
+        levelTickCount++;
+        tickCount++;
     }
-    protected  void resetLevelTick() { levelTick = 0L; }
+    protected  void resetLevelTick() { levelTickCount = 0L; }
 
     protected static void stop() {
-        tickThread.stop(); // This is bad but it works for now.
+        get().running = false; // This is bad but it works for now.
     }
 
     private GameClock() {
-        tick = 0;
-        levelTick = 0;
-        time = 0;
-        levelTime = 0;
+        tickCount = 0;
+        levelTickCount = 0;
+        timePlayed = 0;
+        timeRemaining = 0;
+        timedElements = new ArrayList<>();
+        running = true;
         tickThread.start();
     }
-
 }
---
+++ src/nz/ac/vuw/ecs/swen225/gp22/app/Observer.java
@@ -0,0 +1,5 @@
+package nz.ac.vuw.ecs.swen225.gp22.app;
+
+interface Observer {
+    void update();
+}

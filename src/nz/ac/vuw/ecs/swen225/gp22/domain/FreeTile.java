2d00cc38bee1af011188f28070ef63265119e635
Micky Snadden <snaddemick@ecs.vuw.ac.nz>
2022-09-18 16:53:23 +1200

    made freetile have a constructor without coords as they shouldn't be required to create one I don't think

--- src/nz/ac/vuw/ecs/swen225/gp22/domain/FreeTile.java
+++ src/nz/ac/vuw/ecs/swen225/gp22/domain/FreeTile.java
@@ -1,12 +1,17 @@
 package nz.ac.vuw.ecs.swen225.gp22.domain;
 
+import java.util.Optional;
+
 public class FreeTile {
 	public final Coord coords;
 	
 	public FreeTile(Coord coords) {
 		this.coords = coords;
 	}
-	
+	public FreeTile(){
+		this.coords = null;
+	}
+
 	public boolean onMoveInto(Entity e, Direction d) {
 		return true;
 	}

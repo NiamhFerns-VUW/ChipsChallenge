e4b3b7d5ed9877c4c9c7f4c265150754d4d47ec3
Micky Snadden <snaddemick@ecs.vuw.ac.nz>
2022-09-19 13:16:39 +1200

    made helper methods (that can be heavily refactored ) as well as entityToXmlElement method which uses them

--- src/nz/ac/vuw/ecs/swen225/gp22/persistency/Converter.java
+++ src/nz/ac/vuw/ecs/swen225/gp22/persistency/Converter.java
@@ -1,10 +1,14 @@
 package nz.ac.vuw.ecs.swen225.gp22.persistency;
 
 import com.google.common.base.CaseFormat;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.lang.reflect.Parameter;
 import java.util.Arrays;
+import java.util.Objects;
 import java.util.Optional;
 import java.util.stream.Stream;
 import nz.ac.vuw.ecs.swen225.gp22.domain.*;
@@ -12,10 +16,38 @@ import org.apache.commons.text.CaseUtils;
 import org.dom4j.Document;
 import org.dom4j.DocumentHelper;
 import org.dom4j.Element;
+import org.dom4j.io.OutputFormat;
+import org.dom4j.io.XMLWriter;
 import org.dom4j.util.StringUtils;
 
 public class Converter {
 
+    public static Stream<Method> getStringSetters(Object object) {
+        Document document = DocumentHelper.createDocument();
+        Field[] declaredFields = object.getClass().getDeclaredFields();
+        Method[] declaredMethods = object.getClass().getDeclaredMethods();
+        Stream<Field> stringFields = Arrays.stream(declaredFields).filter(e -> e.getType().getSimpleName().equals("String"));
+        return Arrays.stream(declaredMethods).filter(method -> {
+            if (method.getName().startsWith("set")) {
+                return method.getParameters().length == 1 && method.getParameters()[0].getType()
+                    .getSimpleName().equals("String");
+            }
+            return false;
+        });
+    }
+
+    public static Stream<Method> getStringGetters(Object object) {
+        Document document = DocumentHelper.createDocument();
+        Field[] declaredFields = object.getClass().getDeclaredFields();
+        Method[] declaredMethods = object.getClass().getDeclaredMethods();
+        Stream<Field> stringFields = Arrays.stream(declaredFields).filter(e -> e.getType().getSimpleName().equals("String"));
+        return Arrays.stream(declaredMethods).filter(method -> {
+            if (method.getName().startsWith("get")) {
+                return method.getReturnType().getSimpleName().equals("String");
+            }
+            return false;
+        });
+    }
     public static Stream<Field> getValidStringFields(Object object) {
         Document document = DocumentHelper.createDocument();
         Field[] declaredFields = object.getClass().getDeclaredFields();
@@ -57,7 +89,28 @@ public class Converter {
     // xmlElementToEntity
 
     public static Optional<Element> entityToXmlElement(Entity entity) {
-        return Optional.empty();
+        Document document = DocumentHelper.createDocument();
+        Stream<Field> validStringFields = getValidStringFields(entity);
+        String simpleName = entity.getClass().getSimpleName();
+        System.out.println(simpleName);
+        Element entityElement = document.addElement("entity");
+        entityElement.addElement("name").addText(simpleName);
+        validStringFields.forEach(stringField->{
+            String name = stringField.getName();
+            System.out.println(name);
+            Stream<Method> stringGetters = getStringGetters(entity);
+            Method method = stringGetters.findFirst().orElse(null);
+            try {
+                Object invoke = Objects.requireNonNull(method).invoke(entity);
+                String invokeString = invoke.toString();
+                entityElement.addElement(name).addText(invokeString);
+            } catch (IllegalAccessException e) {
+                throw new RuntimeException(e);
+            } catch (InvocationTargetException e) {
+                throw new RuntimeException(e);
+            }
+        });
+        return Optional.of(entityElement);
     }
 
     // tileToXMLElement

c7705f90d5d14c67b5d11ea664fd42727159cce5
Micky Snadden <snaddemick@ecs.vuw.ac.nz>
2022-09-19 14:47:53 +1200

    barebones xmlElementToEntity

--- src/nz/ac/vuw/ecs/swen225/gp22/persistency/Converter.java
+++ src/nz/ac/vuw/ecs/swen225/gp22/persistency/Converter.java
@@ -16,6 +16,8 @@ import org.apache.commons.text.CaseUtils;
 import org.dom4j.Document;
 import org.dom4j.DocumentHelper;
 import org.dom4j.Element;
+import org.dom4j.Namespace;
+import org.dom4j.dom.DOMNodeHelper;
 import org.dom4j.io.OutputFormat;
 import org.dom4j.io.XMLWriter;
 import org.dom4j.util.StringUtils;
@@ -111,6 +113,51 @@ public class Converter {
             }
         });
         return Optional.of(entityElement);
+    }
+    public static Optional<Entity> xmlElementToEntity(Element element) {
+        System.out.println("xmlElementToEntity on: " + element);
+//        Stream<Element> elementStream = element.elements().stream();
+        Optional<Element> optionalNameElement = element.elements().stream().filter(e -> e.getName().equals("name")).findFirst();
+        // V elements to attempt to set
+        Stream<Element> elementStream = element.elements().stream()
+            .filter(e -> !(e.getName().equals("name")));
+        if (optionalNameElement.isPresent()) {
+            Element nameElement = optionalNameElement.get();
+            String nameElementText = nameElement.getText();
+            try {
+                Class<?> aClass = Class.forName("nz.ac.vuw.ecs.swen225.gp22.domain."+nameElementText);
+                Object o = aClass.getDeclaredConstructor().newInstance();
+                Entity entity = (Entity) o;
+                Stream<Method> stringSetters = getStringSetters(entity);
+                elementStream.forEach(e->{
+                    System.out.println("e: " + e);
+                    System.out.println(e.getName());
+                    Optional<Method> first = stringSetters.filter(
+                        e2 -> e2.getName().substring(3).equalsIgnoreCase(e.getName())).findFirst();
+                    if (first.isPresent()) {
+                        Method method = first.get();
+                        try {
+                            method.invoke(entity,e.getName());
+                        } catch (IllegalAccessException ex) {
+                            throw new RuntimeException(ex);
+                        } catch (InvocationTargetException ex) {
+                            throw new RuntimeException(ex);
+                        }
+                    }
+                });
+            } catch (ClassNotFoundException e) {
+                throw new RuntimeException(e);
+            } catch (InvocationTargetException e) {
+                throw new RuntimeException(e);
+            } catch (InstantiationException e) {
+                throw new RuntimeException(e);
+            } catch (IllegalAccessException e) {
+                throw new RuntimeException(e);
+            } catch (NoSuchMethodException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        return Optional.empty();
     }
 
     // tileToXMLElement

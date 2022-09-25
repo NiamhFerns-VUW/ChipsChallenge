b98717813f32bf1de76adcfd93f79a5b927fbff0
Micky Snadden <snaddemick@ecs.vuw.ac.nz>
2022-09-25 13:20:10 +1300

    added bare necessities for GameSave

--- src/nz/ac/vuw/ecs/swen225/gp22/persistency/GameSave.java
+++ src/nz/ac/vuw/ecs/swen225/gp22/persistency/GameSave.java
@@ -1,5 +1,14 @@
 package nz.ac.vuw.ecs.swen225.gp22.persistency;
 
+import java.util.List;
+import nz.ac.vuw.ecs.swen225.gp22.domain.Cell;
+import nz.ac.vuw.ecs.swen225.gp22.domain.Entity;
+
 public class GameSave {
+
+    public Cell[][] cells;
+    public int time;
+    public List<Entity> inventory;
+    public GameSave(){}
 
 }

f556e290f0852e3bb153ca38886739203c9a1233
zhanghaos <zhanghaos@ecs.vuw.ac.nz>
2022-09-27 00:44:42 +1300

    update

---
+++ src/test/nz/ac/vuw/ecs/swen225/gp22/fuzz/Fuzz.java
@@ -0,0 +1,39 @@
+package nz.ac.vuw.ecs.swen225.gp22.fuzz;
+package nz.ac.vuw.ecs.swen225.gp22.app;
+
+import java.util.ArrayList;
+import java.util.List;
+import nz.ac.vuw.ecs.swen225.gp22.app.*;
+
+import java.awt.AWTException;
+import games.GameHandler;
+import java.awt.Robot;
+import java.awt.event.KeyEvent;
+import java.io.*;
+
+class Fuzz{
+    static final Random random = new Random();
+    public static void randomKeys(int size){
+        Main newGame = new Main();
+        Robot robot = new Robot();
+
+        // newGame.run().newApp();  // skip to first level
+
+        List<KeyEvent> keys = List.of(  KeyEvent.VK_UP,
+                                        KeyEvent.VK_DOWN,
+                                        KeyEvent.VK_LEFT,
+                                        KeyEvent.VK_RIGHT );
+
+        robot.delay(100);
+
+        for (int i = 0; i < size; i++) {
+            robot.keyPress(keys.get(random.nextInt(keys.size())));
+            robot.keyRelease(keys.get(random.nextInt(keys.size())));
+            robot.delay(100);
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException, AWTException {
+        randomKeys(100);
+    }
+}

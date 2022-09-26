88d50fec9baa62c2f852de21d6b203e8216320e1
Micky Snadden <snaddemick@ecs.vuw.ac.nz>
2022-09-27 10:39:35 +1300

    made static loadGameSave and saveGameSave methods which are more or less wrappers around file handler, only working on correct inputs but this is just for integration day

--- src/nz/ac/vuw/ecs/swen225/gp22/persistency/Persistency.java
+++ src/nz/ac/vuw/ecs/swen225/gp22/persistency/Persistency.java
@@ -1,5 +1,22 @@
 package nz.ac.vuw.ecs.swen225.gp22.persistency;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.nio.file.Path;
+import org.dom4j.Document;
+import org.dom4j.Element;
+
 public class Persistency {
 
+    // load gameSave
+    public static GameSave loadGameSave(Path path) {
+        Document document = FileHandler.getXML(path);
+        GameSave gameSave = new GameSave();
+        gameSave.fromXml(document.getRootElement());
+        return gameSave;
+    }
+    // save gameSave
+    public static void saveGameSave(GameSave gameSave, Path path) {
+        FileHandler.saveXML(gameSave.toXml(),path.toString());
+    }
 }

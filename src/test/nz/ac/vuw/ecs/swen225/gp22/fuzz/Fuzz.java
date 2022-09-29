62d932aa4f0d5b7ce5be8ae6282624c854b717c6
zhanghaos <zhanghaos@ecs.vuw.ac.nz>
2022-09-30 12:21:35 +1300

    Update Fuzz.java

--- src/test/nz/ac/vuw/ecs/swen225/gp22/fuzz/Fuzz.java
+++ src/test/nz/ac/vuw/ecs/swen225/gp22/fuzz/Fuzz.java
@@ -1,33 +1,36 @@
 package nz.ac.vuw.ecs.swen225.gp22.fuzz;
 
-import java.lang.reflect.Field;
-import java.util.ArrayList;
 import java.util.List;
 import nz.ac.vuw.ecs.swen225.gp22.app.*;
-import nz.ac.vuw.ecs.swen225.gp22.domain.Direction;
-
 import java.awt.AWTException;
 import java.awt.Robot;
 import java.awt.event.KeyEvent;
 import java.util.Random;
 
-import java.io.*;
-
 class Fuzz extends GameHandler{
     static final Random random = new Random();
+    // keys to be pressed
     private static List<Integer> keys = List.of(
             KeyEvent.VK_UP,
             KeyEvent.VK_DOWN,
             KeyEvent.VK_LEFT,
             KeyEvent.VK_RIGHT );
-
+    // actions to be performed
     private List<Runnable> actions = List.of(
-            input::mvUp,
-            input::mvDown,
-            input::mvLeft,
-            input::mvRight );
+//            input::mvUp,
+//            input::mvDown,
+//            input::mvLeft,
+//            input::mvRight
+    );
+    /**
+     * @param size the number of random inputs to generate
+     * this method generates a random sequence of inputs
+     *             and then executes them
+     * @throws AWTException
+     * @throws IllegalArgumentException
+     */
+    public static void randomKeys(int size) throws AWTException, IllegalArgumentException {
 
-    public static void randomKeys(int size) throws AWTException {
         GameHandler gHandler = new GameHandler();
         Robot robot = new Robot();
 
@@ -42,7 +45,26 @@ class Fuzz extends GameHandler{
             robot.delay(100);
         }
     }
-
+    /**
+     * This method generates a mouse click at a location
+     */
+    public static void mouseTest(){
+        GameHandler gHandler = new GameHandler();
+        Robot robot = null;
+        try {
+            robot = new Robot();
+        } catch (AWTException e) {
+            e.printStackTrace();
+        }
+        robot.mouseMove(100, 100);
+        robot.mousePress(KeyEvent.BUTTON1_DOWN_MASK);
+        robot.mouseRelease(KeyEvent.BUTTON1_DOWN_MASK);
+    }
+    /**
+     * @param size the number of random inputs to generate
+     * this method generates a random sequence of actions functions from the
+     *             list of actions and then executes them
+     */
     public void actiontest(int size){
         GameHandler game = new GameHandler();
 
@@ -51,21 +73,18 @@ class Fuzz extends GameHandler{
             System.out.println("Action: " + i);
         }
     }
-
-    public void main(String[] args) throws InterruptedException, AWTException {
+    public void main(String[] args) throws IllegalArgumentException, AWTException {
         randomKeys(100);
-        actiontest(100);
+//        actiontest(100);
+//        mouseTest();
     }
-
 
 //    @Test
 //    public void testLevel1() {
 //
 //    }
-//
 //    @Test
 //    public void testLevel2() {
 //
 //    }
-
 }

25a844c15733671a4c671732b0dc7451e66be1eb
James Gordon <gordonjame@ecs.vuw.ac.nz>
2022-09-30 10:08:08 +1300

    Added default constructors for Monster and MoveableBlock.

--- src/nz/ac/vuw/ecs/swen225/gp22/domain/MoveableBlock.java
+++ src/nz/ac/vuw/ecs/swen225/gp22/domain/MoveableBlock.java
@@ -4,8 +4,12 @@ package nz.ac.vuw.ecs.swen225.gp22.domain;
  * Can be pushed around by moving into it - it blocks movement if it cannot be pushed forwards.
  */
 public class MoveableBlock extends MovingEntity {
+
 	public MoveableBlock() {
-		super(Direction.None);
+		super(Direction.None, new Coord(-1, -1));
+	}
+	public MoveableBlock(Coord c) {
+		super(Direction.None,c);
 	}
 	@Override
 	public boolean interactBefore(MovingEntity e, Direction d, Cell myCell) {

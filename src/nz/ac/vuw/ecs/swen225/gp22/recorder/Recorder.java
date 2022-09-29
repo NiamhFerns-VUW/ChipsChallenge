b627865db203fe945707a1191a8bba41f6cd45d2
Santino Gaeta <gaetasant@myvuw.ac.nz>
2022-09-30 11:54:49 +1300

    Added an autoreplay() method to Replayer, small changes and Javadocs all round

--- src/nz/ac/vuw/ecs/swen225/gp22/recorder/Recorder.java
+++ src/nz/ac/vuw/ecs/swen225/gp22/recorder/Recorder.java
@@ -3,6 +3,7 @@ package nz.ac.vuw.ecs.swen225.gp22.recorder;
 import nz.ac.vuw.ecs.swen225.gp22.app.*;
 import nz.ac.vuw.ecs.swen225.gp22.persistency.*;
 import org.dom4j.Document;
+import org.dom4j.Element;
 
 import java.util.ArrayList;
 import java.util.Stack;
@@ -16,24 +17,44 @@ public class Recorder {
     private boolean startRecording = false;
 
 
+    /**
+     * Initialises List of RecordedLevels and List of moves from currentRecording
+     */
     public Recorder(){
         recordings = new ArrayList<>();
         currentRecording = new ArrayList<>();
     }
 
+    /**
+     * Sets Recorder to start recording inputs from App
+     */
     public void start(){
         startRecording = true;
     }
 
+    /**
+     * Triggers once currentRecording is saved at the end of Level
+     * Clears the currentRecording of all moves from the level, ready for recording new level
+     * Sets currentLevel to zero - should be set at beginning of each level
+     * Sets Recorder to stop recording inputs - in case of a lull period before levels
+     */
     public void reset(){
         currentRecording.clear();
         currentLevel = "zero";
         startRecording = false;
     }
 
+    /**
+     * From App, will let recorder know what level is currently being recorded
+     * @param level - that Player is currently playing
+     */
     public void setLevel(String level){
         this.currentLevel = level;
     }
+
+    /**
+     * Recording when Player moves Chip 'Up'
+     */
     public void up(){
         if(startRecording){
             long time = GameClock.get().currentTime();
@@ -42,6 +63,9 @@ public class Recorder {
         }
     }
 
+    /**
+     * Recording when Player moves Chip 'Down'
+     */
     public void down(){
         if(startRecording){
             long time = GameClock.get().currentTime();
@@ -50,6 +74,9 @@ public class Recorder {
         }
     }
 
+    /**
+     * Recording when Player moves Chip 'Left'
+     */
     public void left(){
         if(startRecording){
             long time = GameClock.get().currentTime();
@@ -58,6 +85,9 @@ public class Recorder {
         }
     }
 
+    /**
+     * Recording when Player moves Chip 'Right'
+     */
     public void right(){
         if(startRecording){
             long time = GameClock.get().currentTime();
@@ -66,6 +96,9 @@ public class Recorder {
         }
     }
 
+    /**
+     * Recording when game updates and Player has not moved
+     */
     public void none(){
         if(startRecording){
             long time = GameClock.get().currentTime();
@@ -73,32 +106,46 @@ public class Recorder {
         }
     }
 
-    /**
-    public Element setGameSave(GameState gs){
-        GameSave gs
-        return gs.toXML();
-    }
-    */
+    /**public Element setGameSave(long time){
+        GameSave gs = new GameSave();
+        gs.time = (int) time;
+        return gs.toXml();
+    }*/
 
+    /**
+     * Reverse inputs Steps from currentRecording into a Stack of Steps to be called at end of level
+     * Then resets the Recorder - ready for recording a new level
+     */
     public void saveRecording(){
-        //Reverse input allSteps into a Stack as each step is being converted into XML
-        Stack<Step> gameHistory = new Stack<Step>();
-        for(int i = currentRecording.size()-1; i <= 0; i--){
+        Stack<Step> gameHistory = new Stack<>();
+        for(int i = currentRecording.size()-1; i >= 0; i--){
             gameHistory.push(currentRecording.get(i));
         }
         recordings.add(gameHistory);
+        System.out.println(currentLevel+" recorded and saved.");
         reset();
     }
 
-    /** LoadRecording()
-     *  that has information about each
-     * @return Replayer that contains all the moves/gamestates for the recorded level
+    /**
+     *  For IntegrationDay will only pull the first Recording from the List
+     * @return Replayer  - automatically plays the Stack of all the moves from the recorded level
      */
     public Replayer loadRecording(){
-
-        //return new Replayer(Stack history);
         return new Replayer(recordings.get(0));
     }
 
+    /**
+     * @return currentRecording - List of moves from the current recorded level
+     */
+    public ArrayList<Step> getCurrentRecording(){
+        return currentRecording;
+    }
+
+    /**
+     * @return recordings - Contains List of Recorded levels
+     */
+    public ArrayList<Stack<Step>> getListOfRecordings(){
+        return recordings;
+    }
 
 }
--- src/nz/ac/vuw/ecs/swen225/gp22/recorder/Replayer.java
+++ src/nz/ac/vuw/ecs/swen225/gp22/recorder/Replayer.java
@@ -2,19 +2,37 @@ package nz.ac.vuw.ecs.swen225.gp22.recorder;
 
 import java.util.Stack;
 
-/**
- *
- */
+
 public class Replayer {
 
     private int replaySpeed = 1;
-    Stack<Step> history;
-    Stack<Step> prev;
+    private Stack<Step> history;
+    private Stack<Step> prev;
     boolean autoReplay = false;
+
 
     public Replayer(Stack<Step> gameHistory){
         history = gameHistory;
-        prev = new Stack<Step>();
+        prev = new Stack<>();
+    }
+
+    /**
+     * Automatic Replay
+     * While the Stack isn't empty will print each move every 2secs
+     * Also pushes each move into the prev Stack in case User wants to go backwards (Backwards not implemented yet)
+     */
+    public void autoReplay(){
+        autoReplay = true;
+        while(!history.isEmpty()){
+            Step currentStep = history.pop();
+            prev.push(currentStep);
+            System.out.println(currentStep.replayerToString());
+            try {
+                Thread.sleep(2000/replaySpeed);
+            } catch (InterruptedException e) {
+                System.out.println("InterruptedException Exception" + e.getMessage());
+            }
+        }
     }
 
     /**
@@ -24,14 +42,9 @@ public class Replayer {
     //if NONE, push(Step Stack.pop()) into other Stack until reaching a different Step
     //If not NONE, Step replay = Stack.pop(); OtherStack.push(replay); replay.draw();
 
-    /**
-     * Automatic Replay
-     */
-    //Set this.autoReplay = true;
-    //while(autoreplay) - true
-    //(time in between each step) / replaySpeed
-
-
+    public Stack<Step> getHistory(){
+        return history;
+    }
 
     public void setReplaySpeed(int newSpeed){
         this.replaySpeed = newSpeed;

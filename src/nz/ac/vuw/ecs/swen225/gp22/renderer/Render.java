a0148a719700655aaec3d1b80a3812534df26789
kmcevoy <kitt.mcevoy@outlook.com>
2022-10-12 11:35:38 +1300

    Change drawing to 9 by 9 area and implement the getTileImage method in Cell from domain

--- src/nz/ac/vuw/ecs/swen225/gp22/renderer/Render.java
+++ src/nz/ac/vuw/ecs/swen225/gp22/renderer/Render.java
@@ -1,16 +1,14 @@
 package nz.ac.vuw.ecs.swen225.gp22.renderer;
 
+import gameImages.Img;
 import nz.ac.vuw.ecs.swen225.gp22.domain.*;
-import nz.ac.vuw.ecs.swen225.gp22.persistency.GameSave;
-import nz.ac.vuw.ecs.swen225.gp22.persistency.Persistency;
-import nz.ac.vuw.ecs.swen225.gp22.renderer.images.*;
 
 import javax.swing.*;
 import java.awt.*;
-import java.nio.file.Path;
 import java.util.ArrayList;
 
 public class Render extends JPanel {
+
     private Domain domain;
    /**
     * Default constructor for an instance of Render
@@ -20,81 +18,114 @@ public class Render extends JPanel {
     }
 
     /**
-     * Passes
+     * Accepts the Domain instance
+     *
+     * @param d Domain instance used for the game
      * */
     public void setUp(Domain d){
         this.domain = d;
     }
 
+    /**
+     * Checks that the domain has been set.
+     *
+     * @return boolean
+     * */
     public boolean domainSet(){
         return (domain != null);
     }
 
-    public void paintComponent(Graphics g) {
-        boolean cells2 = this.domain.getLevel().isPresent();
-        Cell[][] cells;
-        if(cells2){
-            cells = this.domain.getLevel().get().cells;
 
-        GameSave gameSave = Persistency.loadGameSave(Path.of("./src/levels/level1.xml"));
-        Cell[][] cells3 = gameSave.getCells();
-
-        int tileSize = getHeight()/cells.length;
-
+    /**
+     * A method to find the cell that Chip is currently standing on. This method helps with being able
+     * to figure out the cells that need to be drawn and the cells that do not.
+     *
+     * @return Array of two integers, the first being the x co-ordinate and the second is the y co-ordinate
+     * */
+    private ArrayList<Integer> findChip(Cell[][] cells){
+        ArrayList<Integer> chipsCells = new ArrayList<>();
         for(int i=0; i < cells.length; i++){
-            for(int j=0; j < cells[i].length; j++){
-                Cell c = cells[i][j];
-
-                if(c.getStoredTile().getClass().equals(FreeTile.class)){
-                    g.drawImage(Img.Freetile.image, j*tileSize, i*tileSize, tileSize, tileSize, null);
-                }
-                else if(c.getStoredTile().getClass().equals(Wall.class)){
-                    g.drawImage(Img.Walltile.image, j*tileSize, i*tileSize, tileSize, tileSize, null);
-                }
-                else if(c.getStoredTile().getClass().equals(Exit.class)){
-                    g.drawImage(Img.Exittile.image, j*tileSize, i*tileSize, tileSize, tileSize, null);
-                }
-                else if(c.getStoredTile().getClass().equals(ExitLock.class)){
-                    g.drawImage(Img.Exitlock.image, j*tileSize, i*tileSize, tileSize, tileSize, null);
-                }
-                else if(c.getStoredTile().getClass().equals(Info.class)){
-                    g.drawImage(Img.Infotile.image, j*tileSize, i*tileSize, tileSize, tileSize, null);
-                }
-                else if(c.getStoredTile().getClass().equals(LockedDoor.class)){
-                    LockedDoor ld = ((LockedDoor) c.getStoredTile());
-                    switch (ld.lockColour) {
-                        case "Blue" ->
-                                g.drawImage(Img.BlueLockeddoor.image, j * tileSize, i * tileSize, tileSize, tileSize, null);
-                        case "Green" ->
-                                g.drawImage(Img.GreenLockeddoor.image, j * tileSize, i * tileSize, tileSize, tileSize, null);
-                        case "Yellow" ->
-                                g.drawImage(Img.YellowLockeddoor.image, j * tileSize, i * tileSize, tileSize, tileSize, null);
-                        case "Red" ->
-                                g.drawImage(Img.RedLockeddoor.image, j * tileSize, i * tileSize, tileSize, tileSize, null);
+            for(int j=0; j< cells[i].length; j++){
+                for (Entity e : cells[i][j].getEntities()) {
+                    if (e.getClass().equals(Chip.class)) {
+                        chipsCells.add(i);
+                        chipsCells.add(j);
+                        return chipsCells;
                     }
                 }
-                if(c.getEntities() != null){
-                    ArrayList entities = (ArrayList) c.getEntities();
-                    for(int k=0; k<c.getEntities().size(); k++){
-                        if(entities.get(k).getClass().equals(Chip.class)){
-                            g.drawImage(Img.Chip.image, j * tileSize, i * tileSize, tileSize, tileSize, null);
+            }
+        }
+        return null;
+    }
 
-                        }
-                        if(entities.get(k).getClass().equals(Key.class)) {
-                            Key key = (Key) entities.get(k);
+    /**
+     * This is the method that draws all the tiles, entities and characters
+     *
+     * @param g Graphics pannel to draw to
+     * */
+    public void paintComponent(Graphics g) {
+        boolean cellsPresent = this.domain.getLevel().isPresent();
+        Cell[][] cells;
+        if(cellsPresent){
+            cells = this.domain.getLevel().get().cells;
 
-                            switch (key.keyColour) {
-                                case "Blue" -> g.drawImage(Img.Bluekey.image, j * tileSize, i * tileSize, tileSize, tileSize, null);
-                                case "Green" -> g.drawImage(Img.Greenkey.image, j * tileSize, i * tileSize, tileSize, tileSize, null);
-                                case "Yellow" -> g.drawImage(Img.Yellowkey.image, j * tileSize, i * tileSize, tileSize, tileSize, null);
-                                case "Red" -> g.drawImage(Img.Redkey.image, j * tileSize, i * tileSize, tileSize, tileSize, null);
+            int width = 9;
+            int tileSize = getWidth()/ width;
+
+            ArrayList<Integer> chipsCells = findChip(cells);
+
+            int i=0;
+            int j=0;
+            for(int col=-4; col < 5; col++){
+                for(int row=-4; row < 5; row++){
+                    assert chipsCells != null;
+                    if(col + chipsCells.get(0) < 0 || col + chipsCells.get(0) >= cells.length
+                            || row + chipsCells.get(1) < 0 || row + chipsCells.get(1) >= cells.length ){
+                        g.drawImage(Img.Freetile.image, j*tileSize, i*tileSize, tileSize, tileSize, null);
+                    }
+                    else {
+                        Cell c = cells[col + chipsCells.get(0)][row + chipsCells.get(1)];
+
+                        g.drawImage(c.getTileImage(), j * tileSize, i * tileSize, tileSize, tileSize, null);
+
+//                        if(c.getEntities() != null) {
+//                            ArrayList<Entity> entities = (ArrayList<Entity>) c.getEntities();
+//                            for (int k = 0; k < entities.size(); k++) {
+//                                if (entities.get(k).getClass().equals(Chip.class)) {
+//                                    Info cellI = (Info) c.getStoredTile();
+//                                    String info = cellI.infoText;
+//                                    g.setColor(Color.yellow);
+//                                    g.setFont(new Font("SansSerif", Font.PLAIN, 40));
+//                                    g.drawString(info, 150, 150);
+//                                }
+//                            }
+//                        }
+                        if(c.getEntities() != null){
+                            ArrayList<Entity> entities = (ArrayList<Entity>) c.getEntities();
+                            for(int k=0; k<entities.size(); k++){
+                                if(entities.get(k).getClass().equals(Chip.class)){
+                                    g.drawImage(Img.Chip.image, j*tileSize, i*tileSize, tileSize, tileSize, null);
+                                }
+                                if(entities.get(k).getClass().equals(Key.class)) {
+                                    Key key = (Key) entities.get(k);
+                                    switch (key.keyColour) {
+                                        case "Blue" -> g.drawImage(Img.Bluekey.image, j*tileSize, i*tileSize, tileSize, tileSize, null);
+                                        case "Green" -> g.drawImage(Img.Greenkey.image, j*tileSize, i*tileSize, tileSize, tileSize, null);
+                                        case "Yellow" -> g.drawImage(Img.Yellowkey.image, j*tileSize, i*tileSize, tileSize, tileSize, null);
+                                        case "Red" -> g.drawImage(Img.Redkey.image, j*tileSize, i*tileSize, tileSize, tileSize, null);
+                                    }
+                                }
+                                if(entities.get(k).getClass().equals(Treasure.class)){
+                                    g.drawImage(Img.Treasure.image, j*tileSize, i*tileSize, tileSize, tileSize, null);
+                                }
                             }
                         }
                     }
+                    j++;
                 }
-
+                j=0;
+                i++;
             }
-        }
         }
     }
 }

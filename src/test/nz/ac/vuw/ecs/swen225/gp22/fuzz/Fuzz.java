16964ad1c58a45005b517c37b77428a4ade934cf
zhanghaos <zhanghaos@ecs.vuw.ac.nz>
2022-10-12 14:49:05 +1300

    Update Fuzz.java

--- src/test/nz/ac/vuw/ecs/swen225/gp22/fuzz/Fuzz.java
+++ src/test/nz/ac/vuw/ecs/swen225/gp22/fuzz/Fuzz.java
@@ -1,6 +1,5 @@
 package nz.ac.vuw.ecs.swen225.gp22.fuzz;
 
-import java.lang.reflect.Field;
 import java.util.List;
 import nz.ac.vuw.ecs.swen225.gp22.app.*;
 import java.awt.AWTException;
@@ -140,18 +139,18 @@ public class Fuzz{
         Fuzz f = new Fuzz();
         //use comment and uncomment to switch between random keys and actions
 
-//        InputStrategy inputStrategy = new ProbInput();
-//        f.testInputStrategy(inputStrategy, 100000, "level1");
+        InputStrategy inputStrategy = new ProbInput();
+        f.testInputStrategy(inputStrategy, 100000, "level1");
 
 //        f.randomKeys(10000, "level1");
-        f.actiontest(100000, "level1");
+        //f.actiontest(100000, "level1");
     }
      @Test
     public void test2() throws AWTException {
         Fuzz f = new Fuzz();
 
         // use comment and uncomment to switch between random keys and actions
-//        f.randomKeys(10000, "level1");
+        f.randomKeys(10000, "level1");
 //        f.actiontest(100000, "level2");
     }
 }

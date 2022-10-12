d29269c71bb347efbbe287f2a065fb5a39fcf5fe
Micky Snadden <snaddemick@ecs.vuw.ac.nz>
2022-10-12 16:58:13 +1300

    added javadocs to CustomMOvingEntityService, Closes #50

--- src/nz/ac/vuw/ecs/swen225/gp22/persistency2/custom/CustomMovingEntityService.java
+++ src/nz/ac/vuw/ecs/swen225/gp22/persistency2/custom/CustomMovingEntityService.java
@@ -1,3 +1,6 @@
+/**
+ * @author Micky Snadden
+ */
 package nz.ac.vuw.ecs.swen225.gp22.persistency2.custom;
 
 import java.util.Iterator;
@@ -6,15 +9,34 @@ import nz.ac.vuw.ecs.swen225.gp22.domain.Coord;
 import nz.ac.vuw.ecs.swen225.gp22.domain.Direction;
 import nz.ac.vuw.ecs.swen225.gp22.domain.MovingEntity;
 
+/**
+ * Abstract class Service Providers need to implement.
+ */
 public abstract class CustomMovingEntityService extends MovingEntity {
 
     private Iterator<Direction> iterator;
+
+    /**
+     *
+     * @param facingDir direction entity is facing.
+     * @param coords coordinates of the moving entity.
+     */
     public CustomMovingEntityService(Direction facingDir, Coord coords) {
         super(facingDir, coords);
         this.iterator = directionList().listIterator();
     }
+
+    /**
+     * Houses the directions the custom moving entity will go.
+     * @return
+     */
     public abstract List<Direction> directionList();
 
+    /**
+     * Used to get the next direction for the custom moving entity from the direction list,
+     * looping if at the end of list.
+     * @return next direction the entity should go.
+     */
     public Direction getNextDirection() {
         assert !directionList().isEmpty();
         if (!iterator.hasNext()) {
